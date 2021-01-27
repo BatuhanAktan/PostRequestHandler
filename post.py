3ccf3b56f4b1deff70273decdde6bb343aac6bed
BatuhanAktan <43014701+BatuhanAktan@users.noreply.github.com>
2021-01-27 06:05:27 -0500

    Post Update

--- post.py
+++ post.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('https://requesthandlerbatuhan.herokuapp.com/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def add_message():
     content = request.json
     print(content['id'])
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run()
